e43cf3163c5bba6bd6d6762e0c76e50a6fce9881
BO-RUEI CHEN <boruei.brc@gmail.com>
2023-01-07 23:28:18 +0800

    feat: 擴充 includeHTML 函式
    函式增加參數。（使用該函式需帶入 Callback Function 作為參數，當 Include HTML 後將立即執行 Callback Function）

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,4 +1,4 @@
-function includeHTML () {
+function includeHTML (callback) {
   var z, i, elmnt, file, xhttp;
   z = document.getElementsByTagName('*');
   for (i = 0; i < z.length; i += 1) {
@@ -11,7 +11,7 @@ function includeHTML () {
           if (this.status == 200) { elmnt.innerHTML = this.responseText; }
           if (this.status == 404) { elmnt.innerHTML = 'Page not found.'; }
           elmnt.removeAttribute('include-html');
-          includeHTML();
+          includeHTML(callback);
         }
       }
       xhttp.open('GET', file, true);
@@ -19,8 +19,5 @@ function includeHTML () {
       return;
     }
   }
+  if (callback) callback();
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-  includeHTML();
-});
